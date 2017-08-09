b7c2a86bb6996aca2801de2c1a22b5886d2aa261
Matthew Downs <mwdowns@gmail.com>
2017-08-09 19:17:22 -0400

    Adds controllers and states for the app

--- rolodex.js
+++ rolodex.js
@@ -7,7 +7,19 @@ app.config(function($stateProvider, $urlRouterProvider) {
         name: 'home',
         url: '/',
         templateUrl: 'rolodex.html',
-        controller: 'MainController'
+        controller: 'RolodexController'
+    })
+    .state({
+        name: 'targets',
+        url: '/targets',
+        templateUrl: 'targets.html',
+        controller: 'TargetsController'
+    })
+    .state({
+        name: 'newtarget',
+        url: '/newtarget',
+        templateUrl: 'newtarget.html',
+        controller: 'NewTargetController'
     });
     
     $urlRouterProvider.otherwise('/');
@@ -17,7 +29,34 @@ app.config(function($stateProvider, $urlRouterProvider) {
 app.controller('MainController', function($scope, $stateParams, $state, $rootScope) {
     
     console.log('in the controller');
-    $scope.message = 'hello';
-    console.log('this is the message: ', $scope.message);
+    $scope.loggedIn = true;
+    $scope.username = 'Matt';
     
 });
+
+app.controller('RolodexController', function($scope, $stateParams, $state, $rootScope) {
+    
+    $scope.newTargetMessage = 'click to add new target';
+    $scope.viewTargetsMessage = 'click to see all targets';
+    
+    $scope.addNewTarget = function() {
+        console.log('clicked to add new target');
+        $state.go('newtarget');
+    };
+    
+    $scope.viewTargets = function() {
+        console.log('clicked to view targets');
+        $state.go('targets');
+    };
+    
+});
+
+app.controller('NewTargetController', function($scope, $stateParams, $state, $rootScope) {
+    console.log('making a new target');
+});
+
+app.controller ('TargetsController', function($scope, $stateParams, $state, $rootScope) {
+    console.log('looking at all targets');
+});
+
+
