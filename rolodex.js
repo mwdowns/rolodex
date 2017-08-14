6931a11cb14f98057d734f497a1030854f7689d5
Matthew Downs <mwdowns@gmail.com>
2017-08-14 17:49:57 -0400

    Should have committed earlier...styling changes, and adds the "add new contact" functionality to the add target and edit target page

--- rolodex.js
+++ rolodex.js
@@ -39,6 +39,9 @@ app.factory('rolodexService', function($rootScope, $state, $http){
     
     $rootScope.home = true;
     
+    $rootScope.statusList = ["Researching", "Pending-Approval", "Approved", "Declined"];
+    $rootScope.finacialPerfList = ["Best", "OK", "Sub-Par"];
+    
     $rootScope.companies = [
         {
             id: 1,
@@ -46,15 +49,16 @@ app.factory('rolodexService', function($rootScope, $state, $http){
                 companyName: "Blah.co",
                 numOfEmployees: 50
             },
-            companyStatus: 'researching',
+            companyStatus: 'Researching',
             keyContacts: [
                 {
+                    contactID: 1,
                     contactName: 'Barbara',
                     contactPhone: '555-555-5555',
                     contactEmail: 'barbara@blah.com'
                 }
             ],
-            financialPerf: 'best'
+            financialPerf: 'Best'
         },
         {
             id: 2,
@@ -62,20 +66,22 @@ app.factory('rolodexService', function($rootScope, $state, $http){
                 companyName: "SuperLLC",
                 employees: 400
             },
-            companyStatus: 'pending-approval',
+            companyStatus: 'Pending-Approval',
             keyContacts: [
                 {
+                    contactID: 1,
                     contactName: 'Stan',
                     contactPhone: '444-444-4444',
                     contactEmail: 'stan@super.com'
                 },
                 {
+                    contactID: 2,
                     contactName: 'Sue',
                     contactPhone: '444-444-1111',
                     contactEmail: 'sue@super.com'
                 }
             ],
-            financialPerf: 'sup-par'
+            financialPerf: 'Sub-Par'
         },
         {
             id: 3,
@@ -83,9 +89,10 @@ app.factory('rolodexService', function($rootScope, $state, $http){
                 companyName: "OK.inc",
                 employees: 9000
             },
-            companyStatus: 'declined',
+            companyStatus: 'Declined',
             keyContacts: [
                 {
+                    contactID: 1,
                     contactName: 'Oliver',
                     contactPhone: '222-222-2222',
                     contactEmail: 'oliver@ok.com'
@@ -138,6 +145,14 @@ app.controller('MainController', function($scope, $stateParams, $state, $rootSco
     $scope.loggedIn = true;
     $scope.username = 'Matt';
     
+    $scope.logOut = function() {
+        $scope.loggedIn = false;
+    };
+    
+    $scope.logIn = function() {
+        $scope.loggedIn = true;
+    };
+    
 });
 
 //Controller for the main landing page. Just a couple messages.
@@ -145,14 +160,16 @@ app.controller('RolodexController', function($scope, $rootScope, $state, $stateP
     
     console.log($scope.companies.length);
     
-    $scope.newTargetMessage = 'click to add new target';
-    $scope.viewTargetsMessage = 'click to see all targets';
+    $scope.newTargetMessage = "I feel like acquiring today!";
+    $scope.viewTargetsMessage = "I want to see who I want to acquire";
     
 });
 
 // Controller for viewing all the targets
 app.controller('TargetsController', function($scope, $rootScope, $state, $stateParams, rolodexService) {
     console.log('looking at all targets: ', $scope.companies);
+    
+    $rootScope.home = false;
     
     // Function for when the Edit button is clicked under a particular target
     $scope.editButton = function(id) {
@@ -166,6 +183,7 @@ app.controller('TargetsController', function($scope, $rootScope, $state, $stateP
 //Controller for adding a new target
 app.controller('AddTargetController', function($scope, $rootScope, $state, $stateParams, rolodexService) {
     
+    $rootScope.home = false;
     // This little bit of code gets all the IDs, finds the highest one and then adds 1 to that to get the ID for the new entry.
     var IDs = [];
     for (i = 0; i < $rootScope.companies.length; i++) {
@@ -173,8 +191,44 @@ app.controller('AddTargetController', function($scope, $rootScope, $state, $stat
     }
     var newID = Math.max(...IDs) + 1;
     
+    $scope.numOfContacts = [{
+        contactID: 1,
+        contactName: '',
+        contactPhone: '',
+        contactEmail: ''
+    }];
+    
+    // Function for adding a new set of inputs for a new contactEmail
+    // Updates the id based on the number of contacts in the arrary
+    $scope.addAnotherContact = function() {
+        
+        var newIndex = $scope.numOfContacts.length + 1;
+        var newContact = {
+            contactID: newIndex,
+            contactName: '',
+            contactPhone: '',
+            contactEmail: ''
+        };
+        $scope.numOfContacts.push(newContact);
+    };
+    
     //This is the function that the form uses to make a new target
     $scope.addTarget = function() {
+        
+        // This little bit here takes care of the contacts section
+        // which can get hairy if you're adding more than one -- it 
+        // creates an array of objects for however many contacts you've added
+        var contacts = [];
+        for (i = 0; i < $scope.numOfContacts.length; i++) {
+            var contact = {
+                contactID: $scope.numOfContacts[i].contactID,
+                contactName: $scope.numOfContacts[i].contactName,
+                contactPhone: $scope.numOfContacts[i].contactPhone,
+                contactEmail: $scope.numOfContacts[i].contactEmail,
+            };
+            contacts.push(contact);
+        }
+        
         //Creates a new target object with the info entered into the form
         var newTarget = {
             id: newID,
@@ -182,15 +236,9 @@ app.controller('AddTargetController', function($scope, $rootScope, $state, $stat
                 companyName: $scope.companyName,
                 employees: $scope.numOfEmployees,
             },
-            companyStatus: $scope.companyStatus,
-            keyContacts: [
-                {
-                    contactName: $scope.contactName,
-                    contactPhone: $scope.contactPhone,
-                    contactEmail: $scope.contactEmail,
-                }
-            ],
-            financialPerf: $scope.financialPerf
+            companyStatus: $scope.statusList,
+            keyContacts: contacts,
+            financialPerf: $scope.financialPerfList
         };
         console.log('adding new target to rootScope, which looks like this now: ', $rootScope.companies);
         // Pushes this new object into the $rootScope.copanies array
@@ -205,6 +253,8 @@ app.controller('AddTargetController', function($scope, $rootScope, $state, $stat
 //Controller for the Edit Target page, for editing a single target.
 app.controller('EditTargetController', function($scope, $rootScope, $state, $stateParams, rolodexService) {
     
+    $rootScope.home = false;
+    
     // Get the company ID from the $statePararms
     $scope.targetID = $stateParams.target_id;
     console.log("i'm editing stuff! And this is the id: ", $scope.targetID);
@@ -212,9 +262,27 @@ app.controller('EditTargetController', function($scope, $rootScope, $state, $sta
     for (i = 0; i < $rootScope.companies.length; i++ ) {
         if ($rootScope.companies[i].id === parseInt($scope.targetID)) {
             $scope.target = $rootScope.companies[i];
+            $scope.arrayOfContacts = [];
+            for (j = 0; j < $rootScope.companies[i].keyContacts.length; j++) {
+                $scope.arrayOfContacts.push($rootScope.companies[i].keyContacts[j]);
+            }
         }
     }
     console.log('this is the company info: ', $scope.target);
+    console.log('these are the contacts: ', $scope.arrayOfContacts);
+    
+    $scope.addAnotherContact = function() {
+        
+        var newIndex = $scope.arrayOfContacts.length + 1;
+        var newContact = {
+            contactID: newIndex,
+            contactName: '',
+            contactPhone: '',
+            contactEmail: ''
+        };
+        $scope.arrayOfContacts.push(newContact);
+        console.log('this is the array of contacts: ', $scope.arrayOfContacts);
+    };
     //This is the the edit function for the edit form submit button. It basically does the forloop again and updates the rootScope array with the info from the edit page, and by updates, I mean it just replaces everything whether it was changed or not.
     $scope.editTarget = function() {
         for (i = 0; i < $rootScope.companies.length; i++ ) {
@@ -224,6 +292,17 @@ app.controller('EditTargetController', function($scope, $rootScope, $state, $sta
                 $rootScope.companies[i].companyInfo.numOfEmployees = $scope.target.companyInfo.numOfEmployees;
                 $rootScope.companies[i].companyStatus = $scope.target.companyStatus;
                 $rootScope.companies[i].financialPerf = $scope.target.financialPerf;
+                var contacts = [];
+                for (j = 0; j < $scope.arrayOfContacts.length; j++) {
+                    var contact = {
+                        contactID: $scope.arrayOfContacts[j].contactID,
+                        contactName: $scope.arrayOfContacts[j].contactName,
+                        contactPhone: $scope.arrayOfContacts[j].contactPhone,
+                        contactEmail: $scope.arrayOfContacts[j].contactEmail,
+                    };
+                    contacts.push(contact);
+                }
+                $rootScope.companies[i].keyContacts = contacts;
                 //Once all the info is updated, we go back to the list all targets page
                 $state.go('targets');
             }
